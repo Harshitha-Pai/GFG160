91e95e2022abdc299f09e458e830b895a5576017
harshitha-pai <harshithapaiofficial03@gmail.com>
2024-12-02 22:44:17 +0530

    Day 5

---
+++ NextPermutation.java
@@ -0,0 +1,52 @@
+public class NextPermutation{
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 1, 7, 5, 0 };
+        nextPermutation(arr);
+
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+    public static void nextPermutation(int[] arr) {
+        int n = arr.length;
+
+        int pivot = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1) {
+            reverseArr(arr, 0, n - 1);
+            return;
+        }
+
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr, i, pivot);
+                break;
+            }
+        }
+
+        reverseArr(arr, pivot + 1, n - 1);
+    }
+
+    private static void reverseArr(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+}
